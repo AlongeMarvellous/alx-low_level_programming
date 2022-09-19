afe2e43be71260eee9f1200a03b611173012ef40
AlongeMarvellous <alongemarvellous@gmail.com>
2022-09-19 21:18:13 +0100

    e

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,11 +11,11 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s)
-void puts2(char *str);[B[A
-void puts_half(char *str);[A[A[D[D[D[C:[B[B[C[C[D
-void print)[3_array(int *a, int n);
-char *_strcpy(char *dest, char *sec);[D[D[r[C[C[C[C[D
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
 #endif /*MAIN_H*/
